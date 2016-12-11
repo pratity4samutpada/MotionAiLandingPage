89cb8a31645ff60cd79536e11698d7e21a7571c2
dwind5008 <davidhenrywind1989@gmail.com>
2016-12-11 14:37:55 +0200

    final edits

--- js/MotionAi.js
+++ js/MotionAi.js
@@ -13,24 +13,30 @@ $('.box').click(function() {
         }, 500);
 
     if ($(this).next().size() > 0) {
+
         $(this).next().animate({
         left:'50%'
         },500);
+
         }
 
     else {
+
         $(this).prevAll().last().animate({
         left: '50%'
         }, 500);
+
         }
 
 });
 
-
-
 $(document).ready(function() {
+
     $('#showmenu').click(function() {
+
         $('.menu').slideToggle("fast");
+
     });
+
 });
 
